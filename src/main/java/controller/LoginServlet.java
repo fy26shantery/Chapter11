3823d6715227d0da5dc755c1917b987eac130310
t.mori <t.mori@shantery.co.jp>
2026-07-08 11:41:25 +0900

    Chapter11

---
+++ src/main/java/controller/LoginServlet.java
@@ -0,0 +1,96 @@
+package controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import dao.DataManager;
+import dto.ShoutDTO;
+import dto.UserDTO;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+//@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LoginServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+
+	// index.jspの[ログイン]ボタンから呼び出される
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		//送信情報の取得
+		String loginId = request.getParameter("loginId");
+		String userName = request.getParameter("userName");
+		String password = request.getParameter("password");
+
+		RequestDispatcher dispatcher = null;
+		String message = null;
+
+		if (loginId.equals("") || userName.equals("") || password.equals("")) {
+			//ログインIDかパスワード、ユーザ名、どれかが一つでも未入力なら
+			message = "ログインIDとユーザ名・パスワードは必須入力です";
+
+			//エラーメッセージをリクエストオブジェクトに保存
+			request.setAttribute("alert", message);
+
+			//index.jspに処理を転送
+			dispatcher = request.getRequestDispatcher("index.jsp");
+			dispatcher.forward(request, response);
+		} else {
+			//ログイン認証を行い、ユーザ情報を取得
+			DataManager dbm = new DataManager();
+			UserDTO user = dbm.getLoginUser(loginId, userName, password);
+
+			if (user != null) {
+				//ユーザ情報を取得できたら、書き込み内容リストを取得
+				ArrayList<ShoutDTO> list = dbm.getShoutList();
+				HttpSession session = request.getSession();
+
+				//ログインユーザ情報、書き込み内容リストとしてセッションに保存
+				session.setAttribute("user", user);
+				session.setAttribute("shouts", list);
+
+				//処理の転送先をtop.jspに指定
+				dispatcher = request.getRequestDispatcher("top.jsp");
+
+			} else {
+				//ユーザ情報が取得できない場合
+				//エラーメッセージをリクエストオブジェクトに保存
+				message = "ログインID・ユーザ名・パスワードのいずれかが違います";
+				request.setAttribute("alert", message);
+
+				//処理の転送先をindex.jspに指定
+				dispatcher = request.getRequestDispatcher("index.jsp");
+			}
+		}
+		dispatcher.forward(request, response);
+	}
+
+}
---
+++ src/main/webapp/top.jsp
@@ -0,0 +1,90 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+
+<%@ taglib prefix="c" uri="jakarta.tags.core"%>
+<%@ taglib prefix="fmt" uri="jakarta.tags.fmt"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+
+<title>top</title>
+<link rel="stylesheet" href="./css/skyblue.css">
+<link rel="stylesheet" href="./css/pe-icon-7-stroke.css">
+<link rel="stylesheet" href="./css/helper.css">
+
+
+</head>
+<body>
+	<%-- セッションスコープにある UserDTO型のオブジェクトを参照 --%>
+	<jsp:useBean id="user" scope="session" type="dto.UserDTO"></jsp:useBean>
+	<div class="bg-success padding-y-5">
+		<div class="padding-y-5 text-center">
+			<strong>Shouter</strong>
+		</div>
+	</div>
+
+	<div class="padding-y-5">
+		<div style="width: 40%" class="container padding-y-5">
+			<%-- action 属性にサーブレットを指定 --%>
+			<form action="./logout" method="post">
+				<table class="table table-bordered">
+					<tr>
+						<td rowspan="2" class="text-center"><span
+							class="${user.icon } pe-3x pe-va"></span></td>
+						<td width="256">${user.userName }</td>
+						<td><input class="btn btn-light" type="submit" value="ログアウト" /></td>
+
+
+					</tr>
+					<tr>
+						<td colspan="2">${user.profile }</td>
+					</tr>
+
+				</table>
+			</form>
+	
+	<%-- action 属性にサーブレットを指定 --%>
+	<h6 class="text-center">今の気持ちを叫ぼう</h6>
+	<form action="./bbs" method="post">
+		<table class="table">
+			<tr>
+				<%-- 今の気持ち入力欄の名前は shout --%>
+				<td><input class="form-control" type="text" name="shout"
+					value="" size="60" /></td>
+				<td><input class="btn" type="submit" value="叫ぶ" /></td>
+			</tr>
+		</table>
+	</form>
+		</div>
+	</div>
+	
+
+
+	<%-- セッションスコープにあるArrayList型のオブジェクトを参照 --%>
+	<jsp:useBean id="shouts" scope="session"
+		type="java.util.ArrayList<dto.ShoutDTO>" />
+	<div class="padding-y-5">
+		<div style="width: 40%" class="container padding-y-5">
+		<h6 class="text-center">みんなの叫び</h6>
+			<%-- リストにある要素の数だけ繰り返し --%>
+			<c:forEach var="shout" items="${shouts }">
+				<table class="table table-striped table-bordered">
+					<tr>
+						<td rowspan="2" class="text-center"><span
+							class="${shout.icon } pe-3x pe-va"></span></td>
+		<td>${shout.userName }</td>
+		</tr>
+		<tr>
+		<td>${shout.date }</td>
+		</tr>
+		<tr>
+		<td colspan="2"><textarea rows="5" class="form-control">${shout.writing }</textarea>
+		</td>
+		</tr>
+		</table>
+		</c:forEach>
+		</div>
+		</div>
+</body>
+</html>
