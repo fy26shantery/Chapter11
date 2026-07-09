596bd300cd0a3440167c1d079b6830441988e5dd
t.mori <t.mori@shantery.co.jp>
2026-07-09 09:16:09 +0900

    re6

--- src/main/java/controller/LoginServlet.java
+++ src/main/java/controller/LoginServlet.java
@@ -62,14 +62,13 @@ public class LoginServlet extends HttpServlet {
 
 			//index.jspに処理を転送
 			dispatcher = request.getRequestDispatcher("index.jsp");
-			dispatcher.forward(request, response);
+
 		} else if (!(loginId.matches("^[0-9a-zA-Z]+$") && password.matches("^[0-9a-zA-Z]+$"))) {
 
 			//半角英数字のみ
 			message = "半角英数字で入力してください";
 			request.setAttribute("alert", message);
 			dispatcher = request.getRequestDispatcher("index.jsp");
-			dispatcher.forward(request, response);
 
 		} else {
 			//ログイン認証を行い、ユーザ情報を取得
